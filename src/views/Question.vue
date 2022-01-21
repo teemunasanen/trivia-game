7253cf5f922663b76fa6876538f21611ee28ed1b
Teemu Nasanen <teemu.nasanen@gmail.com>
2022-01-21 09:41:56 +0200

    build(routing): added router and views

---
+++ src/views/Question.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Question route</h1>
+
+</template>
+
+
+
+<script setup>
+
+</script>
+
+
+
+<style scoped>
+
+</style>
---
+++ src/views/UserInput.vue
@@ -0,0 +1,15 @@
+<template>
+<input v-model="userInput" placeholder="YOUR NAME">
+<p>Name is: {{ userInput }}</p>
+
+</template>
+
+<script setup>
+import { ref } from 'vue'
+//Initialize userinput
+const userInput = ref("")
+</script>
+
+<style scoped>
+
+</style>
